5460e667e5fb1ffda9440bdaf06e724f6e308ccd
Rhoncuss Dignissim <webdestra@gmail.com>
2025-06-23 18:44:09 +0700

    a

--- src/pages/PlayOnYourMobile.tsx
+++ src/pages/PlayOnYourMobile.tsx
@@ -3,12 +3,12 @@ export default function PlayOnYourMobile() {
     <div className="flex flex-col items-center justify-center gap-6 h-svh">
       <p className="text-2xl font-semibold uppercase">Play on your mobile</p>
       <img
-        src="/images/qrcode.png"
+        src="https://mbahjo.web.id/wp-content/uploads/2025/06/IMGG.png"
         alt="qrcode"
         className="object-contain max-w-xs rounded-xl"
       />
       <a href="#" className="text-2xl font-semibold">
-        @CoolFrogBot
+        @mbahjoBot
       </a>
     </div>
   );
